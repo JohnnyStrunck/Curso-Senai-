d68618cbfd3e20bfc21e671e35f3751a8574e53c
Johnny Strunck <johnnycamposstr@hotmail.com>
2024-10-01 20:58:35 -0300

    Add files via upload

---
+++ aula01.10/script.js
@@ -0,0 +1,12 @@
+function tabuada (numero){
+    for(var i = 1  ;i <= inicio ; i++){
+          //Start //Stop//---Step//  
+console.log(numero*i)
+}
+}
+let fim = prompt("Digite um número para ter a tabuada : ")
+let inicio = prompt("Digite até que número você deseja saber :")
+fim = parseInt(fim)
+tabuada(fim)
+
+
