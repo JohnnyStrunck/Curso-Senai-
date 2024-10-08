b3b0e4cad76a430f22a048bbba1c889c3aff3034
Johnny Strunck <johnnycamposstr@hotmail.com>
2024-10-07 21:52:40 -0300

    Add files via upload

---
+++ aula07.10/script.js
@@ -0,0 +1,37 @@
+// exercicio 1
+
+// let numero = 1;
+
+// while (numero !== 11){
+// console.log(numero)
+// numero++;
+// }
+
+//----Exercicio 2----///
+
+// let anos = 0;
+// let populacaoA = 80000; // taxa anual de crescimento = 2400
+// let populacaoB = 200000; // taxa anual de crescimento = 3000
+
+// while(populacaoA < populacaoB){
+//  populacaoA += populacaoA * 0.03;
+//  populacaoB += populacaoB * 0.015;
+//  anos++;
+
+// }
+// alert(`A populaçao A irá aclcancar a populacao B em ${anos} anos`)
+
+//----Exercicio 3----///
+
+let saldo = 2000;
+let nomeJogo = prompt('Qual o nome do jogo?');
+let valorJogo = Number(prompt('Qual o valor do jogo?'));
+soma = valorJogo + valorJogo 
+
+    while(valorJogo <= dinheiro ){
+
+
+
+saldo++
+    
+}
