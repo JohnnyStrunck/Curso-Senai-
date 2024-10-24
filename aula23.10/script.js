805eaa80b9305522586a56762889addaaf1ef625
Johnny Strunck <johnnycamposstr@hotmail.com>
2024-10-24 19:13:27 -0300

    Add files via upload

---
+++ aula23.10/script.js
@@ -0,0 +1,27 @@
+// function soma(a, b){
+//     return a+b
+
+// }
+
+// console.log(`O resultado da soma é ${soma(10,20)}`)
+
+
+// function subtracao (sub1, sub2){
+//     return sub1-sub2
+// }
+// console.log(`O resultado da subtração é ${subtracao(100,20)}`)
+
+
+
+// function multiplicacao (x,y){
+//         return x*y
+// }
+// console.log(`O resultado da multiplicação é ${multiplicacao(5,5)}`)
+
+
+// function divisao (o , p){
+//     return o/p
+// }
+// console.log(`O resultado a divisão é ${divisao(20,2)}`)
+
+
